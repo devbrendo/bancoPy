e72d24e1be356e9b0f5aac0ac149865257cc019e
devbrendo <93737945+devbrendo@users.noreply.github.com>
2022-02-22 21:11:28 -0300

    correção metodo

--- utils/helper.py
+++ utils/helper.py
@@ -6,7 +6,8 @@ def date_para_str(data: date) -> str:
     return date.strftime('%d/%m/%Y')
 
 def str_para_date(data: str) -> date:
-    return datetime.strftime(data, '%d/%m/%Y')
+    return datetime.strptime(data, '%d/%m/%Y')
 
 def format_float_str_moeda(valor: float) ->str:
     return f'R$ {valor:,.2f}'
+
